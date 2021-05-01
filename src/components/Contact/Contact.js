64e6b4f9389d273c3d8dcf93ca9aa957f702c1a3
Tata Rozhdez <tanya.zhukova.95@gmail.com>
2021-05-01 14:18:16 +0300

    before add meta

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,6 +11,11 @@ export const Contact = ({ t }) => {
   const iframe =
     '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d40659.76249761815!2d30.484912756357538!3d50.436720381373526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4ce454e150021%3A0x72359aa69acc2603!2sZebra%20Lounge!5e0!3m2!1sen!2sua!4v1615839922748!5m2!1sen!2sua" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'
 
+  const mouseMove = (e) => {
+    const pageX = e.clientX - window.innerWidth / 2
+    e.target.style.transform =
+      'rotate(-10deg) translateX(' + +pageX / 50 + '%) translateY(16%)'
+  }
   return (
     <div className='contact'>
       <div className='contact-txt'>
@@ -76,8 +81,8 @@ export const Contact = ({ t }) => {
       <div className='footer-3'>
         <img src={leafImg} alt='footer leaf' />
       </div>
-      <div className='footer-5'>
-        <img src={leafImg} alt='footer leaf' />
+      <div className='footer-4'>
+        <img src={leafImg} alt='footer leaf' onMouseMove={mouseMove} />
       </div>
     </div>
   )
